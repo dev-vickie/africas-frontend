5b74ec86755edf11137b09b0dd49e7a0b7b523bc
dev-vickie <vmutethia84@gmail.com>
2023-04-01 16:56:45 +0300

    wrap column with sinlechild scroll view

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -32,56 +32,58 @@ class _HomePageState extends State<HomePage> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              const SizedBox(height: 100),
-              buildInput("name", nameController),
-              const SizedBox(height: 20),
-              buildInput("Phone Number", phoneController),
-              const SizedBox(height: 20),
-              buildInput("age", ageController),
-              const SizedBox(height: 20),
-              buildInput("department", departmentController),
-              const SizedBox(height: 20),
-              InkWell(
-                onTap: () async {
-                  var message =
-                      "Hello there from ${phoneController.text} my name is ${nameController.text}. I am ${ageController.text} this years old. I am requestin to get a chance to meet the doctor in ${departmentController.text} department";
-                  var client = Client();
-                  try {
-                    var response = await client.post(
-                        Uri.parse(
-                            "https://africas.up.railway.app/${message}/${phoneController.text}"),
-                        headers: {
-                          "Accept": "application/json",
-                          "Content-type": "application/x-www-form-urlencoded"
-                        });
-                    if (response.statusCode == 200) {
-                      print("messae send");
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 100),
+                buildInput("name", nameController),
+                const SizedBox(height: 20),
+                buildInput("Phone Number", phoneController),
+                const SizedBox(height: 20),
+                buildInput("age", ageController),
+                const SizedBox(height: 20),
+                buildInput("department", departmentController),
+                const SizedBox(height: 20),
+                InkWell(
+                  onTap: () async {
+                    var message =
+                        "Hello there from ${phoneController.text} my name is ${nameController.text}. I am ${ageController.text} this years old. I am requestin to get a chance to meet the doctor in ${departmentController.text} department";
+                    var client = Client();
+                    try {
+                      var response = await client.post(
+                          Uri.parse(
+                              "https://africas.up.railway.app/${message}/${phoneController.text}"),
+                          headers: {
+                            "Accept": "application/json",
+                            "Content-type": "application/x-www-form-urlencoded"
+                          });
+                      if (response.statusCode == 200) {
+                        print("messae send");
+                      }
+                    } catch (e) {
+                      print(e.toString());
                     }
-                  } catch (e) {
-                    print(e.toString());
-                  }
-                },
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: const Center(
-                      child: Text(
-                    "Submit",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                  },
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  )),
-                ),
-              )
-            ],
+                    child: const Center(
+                        child: Text(
+                      "Submit",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
